fed33d8880cca1bc32f6f97b260caf3a99eb5bfd
Lurzapps <lurzapps@gmail.com>
2021-01-25 22:58:48 +0100

    split into files

---
+++ train_model.py
@@ -0,0 +1,85 @@
+"""
+train the model sequentially
+"""
+
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import global_variables as gv
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+# plots accuracy and loss curves
+def plot_model_history(model_history):
+    """
+    Plot Accuracy and Loss curves given the model_history
+    """
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    # summarize history for accuracy
+    axs[0].plot(range(1, len(model_history.history['accuracy']) + 1), model_history.history['accuracy'])
+    axs[0].plot(range(1, len(model_history.history['val_accuracy']) + 1), model_history.history['val_accuracy'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1, len(model_history.history['accuracy']) + 1),
+                      len(model_history.history['accuracy']) / 10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1, len(model_history.history['loss']) + 1), model_history.history['loss'])
+    axs[1].plot(range(1, len(model_history.history['val_loss']) + 1), model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1, len(model_history.history['loss']) + 1), len(model_history.history['loss']) / 10)
+    axs[1].legend(['train', 'val'], loc='best')
+    # save the plot and also show
+    fig.savefig('plot.png')
+    plt.show()
+
+
+# directories
+train_dir = 'data/train'
+val_dir = 'data/test'
+
+# define sizes, train and test values
+num_train = 28709
+num_val = 7178
+batch_size = 64
+num_epoch = 50
+
+# define image data generators
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(48, 48),
+    batch_size=batch_size,
+    color_mode="grayscale",
+    class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+    val_dir,
+    target_size=(48, 48),
+    batch_size=batch_size,
+    color_mode="grayscale",
+    class_mode='categorical')
+
+# create the model
+model = gv.build_model()
+
+# start training
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001, decay=1e-6), metrics=['accuracy'])
+model_info = model.fit_generator(
+    train_generator,
+    steps_per_epoch=num_train // batch_size,
+    epochs=num_epoch,
+    validation_data=validation_generator,
+    validation_steps=num_val // batch_size)
+plot_model_history(model_info)
+model.save_weights('model.h5')
---
+++ use_model.py
@@ -0,0 +1,49 @@
+"""
+emotions will be displayed on your face from the webcam feed
+"""
+
+import os
+
+import cv2
+import numpy as np
+
+import global_variables as gv
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# build the model
+model = gv.build_model()
+# and load it into structure
+model.load_weights('model.h5')
+
+# prevents openCL usage and unnecessary logging messages
+cv2.ocl.setUseOpenCL(False)
+
+# start the webcam feed
+cap = cv2.VideoCapture(0)
+while True:
+    # Find haar cascade to draw bounding box around face
+    ret, frame = cap.read()
+    if not ret:
+        break
+    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame, gv.emotion_names[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                    (255, 255, 255),
+                    2, cv2.LINE_AA)
+
+    cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# release open cv cam
+cap.release()
+cv2.destroyAllWindows()
